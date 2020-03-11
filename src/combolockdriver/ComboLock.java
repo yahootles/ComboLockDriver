223101cb8685365bc460a4744c7a39bdb655bf3f
antho6229 <antho6229@7L0M6Y1.Student.UGDSB.ED>
2020-03-11 13:39:44 -0400

    Finished and added comments.

--- src/combolockdriver/ComboLock.java
+++ src/combolockdriver/ComboLock.java
@@ -14,6 +14,12 @@ public class ComboLock {
     int[] combo = new int[3];
     boolean isOpen;
     
+    /**
+     * Constructor that creates a user defined combo lock
+     * @param n1 - first number in the combo
+     * @param n2 - second number in the combo
+     * @param n3 - third number in the combo 
+     */
     public ComboLock(int n1, int n2, int n3){
         combo[0] = n1;
         combo[1] = n2;
@@ -21,6 +27,9 @@ public class ComboLock {
         isOpen = false;
     }
     
+    /**
+     * Constructor that creates random combo
+     */
     public ComboLock(){
         for(int i = 0; i < combo.length; i++){
             combo[i] = (int)Math.round(Math.random()*2+1);
@@ -28,8 +37,13 @@ public class ComboLock {
         isOpen = false;
     }
     
-    public boolean unlock(int n1, int n2, int n3){
-        if(combo[0]==n1 && combo[1] == n2 && combo[2] == n3){
+    /**
+     * Method that returns the state of the lock after comparing the input combo vs the actual combo
+     * @param nums - the input combo
+     * @return - true is combo is correct, false if incorrect
+     */
+    public boolean unlock(int[] nums){
+        if(combo[0]== nums[0] && combo[1] == nums[1] && combo[2] == nums[2]){
             isOpen = true;
             return true;
         }else{
@@ -37,7 +51,23 @@ public class ComboLock {
         }
     }
     
+    /**
+     * Method that locks the lock
+     */
+    public void lock(){
+        isOpen = false;
+    }
+    
+    /**
+     * 
+     * @return - whether the lock is open or not
+     */
     public boolean getIsOpen(){
         return isOpen;
+    }
+    
+    public int[] getCombo(){
+        
+        return combo;
     }
 }
--- src/combolockdriver/ComboLockDriver.java
+++ src/combolockdriver/ComboLockDriver.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Andrew Thompson
+ * March 11, 2020
+ * Program that simulates combo locks that can have random or assigned combos
  */
 package combolockdriver;
 
@@ -17,27 +17,33 @@ public class ComboLockDriver {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //variable declaration
         ComboLock lock1, lock2;
+        //3 input numbers for combo
         int n1 = 0;
         int n2 = 0;
         int n3 = 0;
 
+        //falg for while loop
         boolean flag = false;
+        
+        //get the first number. Must be positive or 0.
         while (!flag) {
             try {
                 n1 = Integer.parseInt(JOptionPane.showInputDialog("Enter the first number in the combo(must be a positive integer):"));
                 if (n1 >= 0) {
                     flag = true;
-                } else {
+                } else {//keep looping if number is negative
                     flag = false;
                 }
-            } catch (NumberFormatException nfe) {
+            } catch (NumberFormatException nfe) {//keep looping until valid input
                 System.err.println("Invalid input");
-            } catch (NullPointerException npe) {
+            } catch (NullPointerException npe) {//exit program if cancel clicked
                 System.exit(0);
             }
         }
 
+        //same as above but with secon number
         flag = false;
         while (!flag) {
             try {
@@ -54,6 +60,7 @@ public class ComboLockDriver {
             }
         }
 
+        //same as above but with third number
         flag = false;
         while (!flag) {
             try {
@@ -70,20 +77,70 @@ public class ComboLockDriver {
             }
         }
 
+        //make new lock with combo as input numbers
         lock1 = new ComboLock(n1, n2, n3);
 
         flag = false;
 
+        //loop until valid input
+        while (!flag) {
+            try {
+                String[] inputs = JOptionPane.showInputDialog("Try to unlock the lock!\nEnter the combo "
+                        + "separated by dashes(ex. 1-3-47)").split("-");//get input and split on dashes
+
+                int[] combo = new int[3];
+                
+                for(int i = 0; i < inputs.length; i++){
+                    combo[i] = Integer.parseInt(inputs[i]);//try to convert to integers
+                }
+                
+                flag = true;
+                
+                if(lock1.unlock(combo)){//try to unlock the lock
+                    System.out.println("The lock is unlocked!");
+                }else{
+                    System.out.println("The lock is not unlocked. The combo is: ");
+                    System.out.println(lock1.getCombo().toString());//tell user combo
+                }
+                
+            } catch (NumberFormatException nfe) {
+                System.err.println("Invalid input. Try again.");
+            } catch (NullPointerException npe) {
+                System.exit(0);
+            } catch (IndexOutOfBoundsException ioobe){
+                System.err.println("Invalid input. Try again.");
+            }
+        }
+        
+        //second lock with random combo
+        lock2 = new ComboLock();
+
+        flag = false;
+
+        //same as first lock
         while (!flag) {
             try {
                 String[] inputs = JOptionPane.showInputDialog("Try to unlock the lock!\nEnter the combo "
                         + "separated by dashes(ex. 1-3-47)").split("-");
 
                 int[] combo = new int[inputs.length];
+                
+                for(int i = 0; i < inputs.length; i++){
+                    combo[i] = Integer.parseInt(inputs[i]);
+                }
+                
+                if(lock2.unlock(combo)){
+                    System.out.println("The lock is unlocked!");
+                }else{
+                    System.out.println("The lock is not unlocked.");
+                }
+                flag = true;
             } catch (NumberFormatException nfe) {
-
+                System.err.println("Invalid input. Try again.");
             } catch (NullPointerException npe) {
                 System.exit(0);
+            } catch (IndexOutOfBoundsException ioobe){
+                System.err.println("Invalid input. Try again.");
             }
         }
     }
